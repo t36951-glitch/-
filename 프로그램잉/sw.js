7f6c670c35182ff72e39c6c6ac7ce1da1026dc71
t36951-glitch <t36951@gmail.com>
2026-03-19 17:33:24 +0900

    메모리 게임

---
+++ 프로그램잉/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'memory-game-v1';
+const urlsToCache = [
+  './memory_game.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Return cached
+        }
+        return fetch(event.request); // Fallback to network
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
